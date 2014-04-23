463cc7cb5d6c7d76bdd2870ce333f5c40dceef57
Sami Andberg <sami.andberg@gmail.com>
2014-04-23 17:06:46 +0800

    ACPCDetect with proper outfile handling etc

--- nipype/interfaces/acpcdetect.py
+++ nipype/interfaces/acpcdetect.py
@@ -4,26 +4,42 @@ from nipype.interfaces.base import (
     CommandLine,
     File
 )
+from nipype.interfaces.traits_extension import isdefined
 import os
 
-class ACPCDInputSpec(CommandLineInputSpec):
-    input_file = File(desc="Infile", exists=True, mandatory=True, argstr="-i %s")
+class ACPCInputSpec(CommandLineInputSpec):
+    infile = File(desc="Infile", exists=True, mandatory=True, argstr='-i %s')
+    outname = File(genfile=True, desc="outfile", position= -1, argstr='-o %s')
 
-class ACPCDOutputSpec(TraitedSpec):
-    output_file = File(desc = "Outfile", exists = True, argstr='-o ac_%s')
+class ACPCOutputSpec(TraitedSpec):
+    outfile = File(desc = "Outfile", exists = True)
 
-class ACPCDTask(CommandLine):
-    input_spec = APCPDInputSpec
-    output_spec = ACPCDOutputSpec
+class ACPCDetect(CommandLine):
+    input_spec = ACPCInputSpec
+    output_spec = ACPCOutputSpec
     cmd = 'acpcdetect'
 
     def _list_outputs(self):
-            outputs = self.output_spec().get()
-            outputs['output_file'] = os.path.abspath("ac_" + self.inputs.input_file)
-            return outputs
+        outputs = self.output_spec().get()
+        outputs['outfile'] = self.inputs.outname
+        if not isdefined(outputs['outfile']):
+            outputs['outfile'] = os.path.abspath(self._gen_outfilename())
+        else:
+            outputs['outfile'] = os.path.abspath(outputs['outfile'])
+        return outputs
+
+    def _gen_filename(self, name):
+        if name is 'outname':
+            return self._gen_outfilename()
+        else:
+            return None
+
+    def _gen_outfilename(self):
+        name = os.path.basename(self.inputs.infile)
+        return 'ac_' + name 
 
 if __name__ == '__main__':
 
-    acd = ACPCDTask(input_file='an_existing_file')
+    acd = ACPCDetect(input_file='an_existing_file')
     print acd.cmdline
     acd.run()
